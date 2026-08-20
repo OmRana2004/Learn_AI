dd3124bbbd3af69adf5258d974772f9529c372a7
OmRana2004 <omrana8755800322@gmail.com>
2026-08-21 00:22:03 +0530

    created-my-first-ever-llm

--- week_1/day1/hello_llm.py
+++ week_1/day1/hello_llm.py
@@ -1,1 +1,25 @@
 import os 
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq   
+
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key kaha hai bhai")
+
+client=Groq(api_key=my_api_key)
+
+model="openai/gpt-oss-120b"
+role="user"
+content="Do you know Harkirat Singh, who teaches coding and own 100x school"
+message={
+    "role": role,
+    "content": content
+}
+
+messages=[message]
+
+response=client.chat.completions.create(model=model, messages=messages)
+print(response.choices[0].message.content)
