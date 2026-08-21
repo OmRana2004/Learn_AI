a48a9921fe8ce59e80c70480388b7fed66ecfd3b
OmRana2004 <omrana8755800322@gmail.com>
2026-08-21 11:33:37 +0530

    system

---
+++ week_1/day2/sys_temp.py
@@ -0,0 +1,31 @@
+import os 
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq   
+
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key kaha hai bhai")
+
+client=Groq(api_key=my_api_key)
+
+model="openai/gpt-oss-120b"
+role="user"
+content="I Love You baby!"
+
+message_system={
+    "role": "system",
+    "content": "You are my loving girlfriend"
+}
+
+message={
+    "role": role,
+    "content": content
+}
+
+messages=[message_system, message]
+
+response=client.chat.completions.create(model=model, messages=messages)
+print(response.choices[0].message.content)
