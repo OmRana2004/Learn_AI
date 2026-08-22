a3a486e57bbb8b72d3f8212cd814c1f07317c990
OmRana2004 <omrana8755800322@gmail.com>
2026-08-22 10:42:06 +0530

    ..

--- hello.js
+++ hello.js
@@ -1,11 +1,16 @@
-const arr = [1, 2, 3, 4, 5]
+// const arr = [1, 2, 3, 4, 5]
  
-newAr = arr.map((sum) => sum+1)
-newArr = arr.map((multiply) => multiply*2)
+// newAr = arr.map((sum) => sum+1)
+// newArr = arr.map((multiply) => multiply*2)
 
-setTimeout(() => {
-console.log(newArr)
-}, 3000)
+// setTimeout(() => {
+// console.log(newArr)
+// }, 3000)
 
 
-console.log(newAr)
+// console.log(newAr)
+
+
+// const value = 5 < 12 ? "NICE" : "NOT NICE";
+// console.log(value)
+
