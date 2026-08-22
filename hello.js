40ce627929439b156c0a748f36ee30fe5197cdb6
OmRana2004 <omrana8755800322@gmail.com>
2026-08-22 11:03:55 +0530

    vote

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 // const arr = [1, 2, 3, 4, 5]
- 
+
 // newAr = arr.map((sum) => sum+1)
 // newArr = arr.map((multiply) => multiply*2)
 
@@ -7,15 +7,24 @@
 // console.log(newArr)
 // }, 3000)
 
-
 // console.log(newAr)
-
 
 // const value = 5 < 12 ? "NICE" : "NOT NICE";
 // console.log(value)
 
-const greeting = "I love You Baby";
+// const greeting = "I love You Baby";
 
-for (let i = 1; i<=20; i++) {
-    console.log(greeting)
+// for (let i = 1; i<=20; i++) {
+//     console.log(greeting)
+// }
+
+const prompt = require("prompt-sync")();
+
+const input = prompt("enter you age :- ");
+const age = Number(input);
+
+if (age >= 18) {
+  console.log("You are eligible");
+} else {
+  console.log("You are not eligible");
 }
